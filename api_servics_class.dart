b0dd30b18600b64ea230b317cc9edb672594d5c4
kenzy991 <145780027+kenzy991@users.noreply.github.com>
2026-04-25 17:05:49 +0300

    Update api_servics_class.dart

--- api_servics_class.dart
+++ api_servics_class.dart
@@ -3,12 +3,9 @@ import 'api_model.dart';
 
 class ApiServicsClass {
   final dio = Dio();
-
   List<ApiModel> posts = [];
-
 Future<void> getPosts() async {
       Response response = await dio.get('https://jsonplaceholder.typicode.com/posts');
-
       List data = response.data;
       posts = data.map((e) => ApiModel.fromJson(e)).toList();
   }
